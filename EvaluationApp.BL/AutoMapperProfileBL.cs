4541c87edc503871e382625f19057f15ad0d17fa
Max <lyuban.maks@gmail.com>
2021-02-05 13:44:15 +0200

    Add BL automapper

---
+++ EvaluationApp.BL/AutoMapperProfileBL.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using EvaluationApp.BL.ModelsBL;
+using EvaluationApp.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluationApp.BL
+{
+    class AutoMapperProfileBL : Profile
+    {
+        public AutoMapperProfileBL()
+        {
+            CreateMap<WebsiteUrl, WebsiteUrlBL>().ReverseMap();
+            CreateMap<UrlResponseStat, UrlResponseStatBL>().ReverseMap();
+        }
+    }
+}
--- EvaluationApp.BL/Services/WebsiteUrlService.cs
+++ EvaluationApp.BL/Services/WebsiteUrlService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using EvaluationApp.BL.ModelsBL;
+using EvaluationApp.DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,31 @@ namespace EvaluationApp.BL.Services
 {
     public class WebsiteUrlService
     {
+        private readonly IMapper _mapper;
 
+        public WebsiteUrlService(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public WebsiteUrl Map(WebsiteUrlBL model)
+        {
+            return _mapper.Map<WebsiteUrl>(model);
+        }
+
+        public WebsiteUrlBL Map(WebsiteUrl model)
+        {
+            return _mapper.Map<WebsiteUrlBL>(model);
+        }
+
+        public IEnumerable<WebsiteUrl> Map(IEnumerable<WebsiteUrlBL> models)
+        {
+            return _mapper.Map<IEnumerable<WebsiteUrl>>(models);
+        }
+
+        public IEnumerable<WebsiteUrlBL> Map(IEnumerable<WebsiteUrl> models)
+        {
+            return _mapper.Map<IEnumerable<WebsiteUrlBL>>(models);
+        }
     }
 }
