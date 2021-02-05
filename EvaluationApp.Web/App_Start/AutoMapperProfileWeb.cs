c920ef5ffdbf0e7f715dd0314242bddcedc090d5
Max <lyuban.maks@gmail.com>
2021-02-05 19:27:30 +0200

    Add Web automapper profile

---
+++ EvaluationApp.Web/App_Start/AutoMapperProfileWeb.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using EvaluationApp.BL.ModelsBL;
+using EvaluationApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EvaluationApp.Web.App_Start
+{
+    public class AutoMapperProfileWeb : Profile
+    {
+        public AutoMapperProfileWeb()
+        {
+            CreateMap<UrlResponseStatBL, UrlResponseStatModel>().ReverseMap();
+            CreateMap<WebsiteUrlBL, WebsiteUrlModel>().ReverseMap();
+        }
+    }
+}
