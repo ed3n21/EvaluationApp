b0733bd114ad4070ee7ffa98ac572123828e306e
Max <lyuban.maks@gmail.com>
2021-02-05 14:08:58 +0200

    Add Web models

---
+++ EvaluationApp.Web/Models/UrlResponseStatModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EvaluationApp.Web.Models
+{
+    public class UrlResponseStatModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(300, MinimumLength = 5)]
+        public string Url { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        [Required]
+        public int ResponseTimeMs { get; set; }
+
+        public int WebsiteUrlId { get; set; }
+        public WebsiteUrlModel WebsiteUrl { get; set; }
+    }
+}
---
+++ EvaluationApp.Web/Models/WebsiteUrlModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EvaluationApp.Web.Models
+{
+    public class WebsiteUrlModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(300, MinimumLength = 5)]
+        public string Url { get; set; }
+    }
+}
