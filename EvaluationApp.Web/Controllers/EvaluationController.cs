f306e0ed065c59366803e98b17110d68ebb3344e
Max <lyuban.maks@gmail.com>
2021-02-08 14:52:26 +0200

    Add xml reader

---
+++ EvaluationApp.Web/Controllers/EvaluationController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using EvaluationApp.BL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace EvaluationApp.Web.Controllers
+{
+    public class EvaluationController : Controller
+    {
+        private readonly WebsiteUrlService _websiteUrlService;
+        private readonly UrlResponseStatService _urlResponseStatService;
+        private readonly IMapper _mapper;
+
+        public EvaluationController(WebsiteUrlService websiteUrlService, UrlResponseStatService urlResponseStatService, IMapper mapper)
+        {
+            _websiteUrlService = websiteUrlService;
+            _urlResponseStatService = urlResponseStatService;
+            _mapper = mapper;
+        }
+
+        public ActionResult Index(string site)
+        {
+            if (String.IsNullOrEmpty(site))
+            {
+                return View();
+            }
+            else
+            {
+                XmlTextReader doc = new XmlTextReader(site + "/sitemap.xml");
+                List<string> urls = new List<string>();
+                while (doc.Read())
+                {
+                    if (doc.NodeType == XmlNodeType.Text)
+                    {
+                        urls.Add(doc.Value.ToString());
+                    }
+                }
+                ViewBag.Sitemap = urls;
+
+                return View("Index", null, site);
+            }
+        }
+    }
+}
